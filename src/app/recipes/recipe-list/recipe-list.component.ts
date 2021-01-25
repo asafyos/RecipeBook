632be7f87f44a97d3f1252fcfaebd4c95d12797d
asafyos <asafsaf3@gmail.com>
2021-01-25 16:59:33 +0200

    Asaf - 25.1.21 - Adding several recipes in list and output them

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,13 +4,28 @@ import { Recipe } from './recipe';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes = [
+    new Recipe(
+      'Pasta',
+      'Yummy Pasta',
+      'https://ministryofcurry.com/wp-content/uploads/2018/07/Pasta-with-Creamy-Tomato-Sauce-1.jpg'
+    ),
+    new Recipe(
+      'Sushi',
+      'Yummy Shusi',
+      'https://media-cdn.tripadvisor.com/media/photo-s/17/5b/c8/d0/shinsen.jpg'
+    ),
+    new Recipe(
+      'Pasta',
+      'Yummy Pasta',
+      'https://4.bp.blogspot.com/-RjhGTGbUBEk/TgHbdYPvPtI/AAAAAAAADc0/E4zNN261-fA/s1600/53.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
