015406fe1c00e4e2f9fe26e30b5088162010df90
asafyos <asafsaf3@gmail.com>
2021-01-26 12:46:56 +0200

    Asaf - 26..1.21 - Adding ingredients to recipes

--- src/app/recipes/recipe-list/recipe.service.ts
+++ src/app/recipes/recipe-list/recipe.service.ts
@@ -1,34 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/Shared/ingredient';
 import { Recipe } from './recipe';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
-
   private recipes = [
     new Recipe(
       'Pasta',
       'Yummy Pasta',
-      'https://ministryofcurry.com/wp-content/uploads/2018/07/Pasta-with-Creamy-Tomato-Sauce-1.jpg'
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/8/26/2/WU0806H_Pasta-with-Tomato-Cream-Sauce_s4x3.jpg.rend.hgtvcom.826.620.suffix/1440693433023.jpeg',
+      [
+        new Ingredient('Pasta', 1),
+        new Ingredient('Tomato', 3),
+        new Ingredient('Salt', 1),
+        new Ingredient('Paper', 1),
+        new Ingredient('Basil', 1),
+      ]
     ),
     new Recipe(
       'Sushi',
       'Yummy Shusi',
-      'https://media-cdn.tripadvisor.com/media/photo-s/17/5b/c8/d0/shinsen.jpg'
+      'https://media-cdn.tripadvisor.com/media/photo-s/17/5b/c8/d0/shinsen.jpg',
+      [
+        new Ingredient('Tuna Fish', 1),
+        new Ingredient('Rice', 1),
+        new Ingredient('Nuri Weed', 1),
+        new Ingredient('Cucamber', 1),
+        new Ingredient('Carrot', 1),
+      ]
     ),
     new Recipe(
       'Pasta',
       'Yummy Pasta',
-      'https://4.bp.blogspot.com/-RjhGTGbUBEk/TgHbdYPvPtI/AAAAAAAADc0/E4zNN261-fA/s1600/53.jpg'
-    )
+      'https://4.bp.blogspot.com/-RjhGTGbUBEk/TgHbdYPvPtI/AAAAAAAADc0/E4zNN261-fA/s1600/53.jpg',
+      [
+        new Ingredient('Tomato Souce', 1),
+        new Ingredient('Mozzarella', 1),
+        new Ingredient('Flour', 1),
+        new Ingredient('Water', 1),
+        new Ingredient('Olive', 25),
+      ]
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-
-  public get Recipes() : Array<Recipe> {
+  public get Recipes(): Array<Recipe> {
     return this.recipes.splice(0);
   }
-
 }
--- src/app/recipes/recipe-list/recipe.ts
+++ src/app/recipes/recipe-list/recipe.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from "src/app/Shared/ingredient";
+
 export class Recipe {
   name: string;
   description: string;
   imagePath: string;
+  ingredients: Array<Ingredient>;
 
-  constructor(name: string, desc: string, imgPath: string) {
+  constructor(name: string, desc: string, imgPath: string, ingedient?: Array<Ingredient>) {
     this.name = name;
     this.description = desc;
     this.imagePath = imgPath;
+    this.ingredients = ingedient || [];
   }
 }
